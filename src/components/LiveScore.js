a4e7a822d2aef82e82107b47ddd2c54e78fa0ba3
Mizanali Panjwani <mizanalip786@gmail.com>
2021-02-02 18:19:54 +0530

    Changed LiveScore from class component to functional component

--- src/components/LiveScore.js
+++ src/components/LiveScore.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Card, CardContent, Typography } from '@material-ui/core'
 import 'fontsource-roboto'
 import SportsCricketRoundedIcon from '@material-ui/icons/SportsCricketRounded'
 import SportsBaseballRoundedIcon from '@material-ui/icons/SportsBaseballRounded'
 
-class LiveScore extends Component {
-    render() {
-        return (
-            <div className='LiveScore'>
-                <Card variant='outlined' style={{width: '90%', margin: '30px auto', backgroundColor: '#424242', color: '#fff', borderRadius: '25px'}}>
-                    <CardContent>
-                        <Typography variant='body1'>INNINGS {this.props.currentInnings} {this.props.target ? <span> - TARGET: {this.props.target}</span> : <span> ({this.props.totalBalls/6} overs)</span>}</Typography>
-                        <br />
-                        <Typography variant='h4' style={{fontWeight: 'bold'}}>{this.props.score}-{this.props.wickets} ({Math.floor(this.props.balls/6)}.{this.props.balls%6})</Typography>
-                        <br />
-                        <Typography variant='h6'><SportsCricketRoundedIcon style={{marginRight: '12px'}} />{this.props.batsman1.name} : {this.props.batsman1.runsScored}({this.props.batsman1.ballsFaced}) {this.props.batsman1.onStrike && this.props.totalWickets !== this.props.wickets ? <span>*</span> : null} </Typography>
-                        <Typography variant='h6'><SportsCricketRoundedIcon style={{marginRight: '12px'}} />{this.props.batsman2.name} : {this.props.batsman2.runsScored}({this.props.batsman2.ballsFaced}) {this.props.batsman2.onStrike && this.props.totalWickets !== this.props.wickets ? <span>*</span> : null} </Typography>
-                        <br />
-                        <Typography variant='h6'><SportsBaseballRoundedIcon style={{marginRight: '12px'}} />{this.props.bowler.name} : {this.props.bowler.wicketsTaken}/{this.props.bowler.runsConceded} ({Math.floor(this.props.bowler.ballsBowled/6)}.{this.props.bowler.ballsBowled%6})</Typography>
-                        {(this.props.target) && (this.props.target>this.props.score) && ((this.props.balls !== this.props.totalBalls) && (this.props.wickets !== this.props.totalWickets)) ? <br /> : null}
-                        {(this.props.target) && (this.props.target>this.props.score) && ((this.props.balls !== this.props.totalBalls) && (this.props.wickets !== this.props.totalWickets)) ? <Typography variant='body1'>TO WIN: {this.props.target - this.props.score} off {this.props.totalBalls - this.props.balls} balls</Typography> : null}
-                    </CardContent>
-                </Card>
-                <div>
-                    <Typography variant='h6'>Ball by Ball:</Typography>
-                    <Typography variant='body1'>{this.props.ballByBall.join(' ')}</Typography>
-                </div>
+const LiveScore = (props) => {
+    return (
+        <div className='LiveScore'>
+            <Card variant='outlined' style={{width: '90%', margin: '30px auto', backgroundColor: '#424242', color: '#fff', borderRadius: '25px'}}>
+                <CardContent>
+                    <Typography variant='body1'>INNINGS {props.currentInnings} {props.target ? <span> - TARGET: {props.target}</span> : <span> ({props.totalBalls/6} overs)</span>}</Typography>
+                    <br />
+                    <Typography variant='h4' style={{fontWeight: 'bold'}}>{props.score}-{props.wickets} ({Math.floor(props.balls/6)}.{props.balls%6})</Typography>
+                    <br />
+                    <Typography variant='h6'><SportsCricketRoundedIcon style={{marginRight: '12px'}} />{props.batsman1.name} : {props.batsman1.runsScored}({props.batsman1.ballsFaced}) {props.batsman1.onStrike && props.totalWickets !== props.wickets ? <span>*</span> : null} </Typography>
+                    <Typography variant='h6'><SportsCricketRoundedIcon style={{marginRight: '12px'}} />{props.batsman2.name} : {props.batsman2.runsScored}({props.batsman2.ballsFaced}) {props.batsman2.onStrike && props.totalWickets !== props.wickets ? <span>*</span> : null} </Typography>
+                    <br />
+                    <Typography variant='h6'><SportsBaseballRoundedIcon style={{marginRight: '12px'}} />{props.bowler.name} : {props.bowler.wicketsTaken}/{props.bowler.runsConceded} ({Math.floor(props.bowler.ballsBowled/6)}.{props.bowler.ballsBowled%6})</Typography>
+                    {(props.target) && (props.target>props.score) && ((props.balls !== props.totalBalls) && (props.wickets !== props.totalWickets)) ? <br /> : null}
+                    {(props.target) && (props.target>props.score) && ((props.balls !== props.totalBalls) && (props.wickets !== props.totalWickets)) ? <Typography variant='body1'>TO WIN: {props.target - props.score} off {props.totalBalls - props.balls} balls</Typography> : null}
+                </CardContent>
+            </Card>
+            <div>
+                <Typography variant='h6'>Ball by Ball:</Typography>
+                <Typography variant='body1'>{props.ballByBall.join(' ')}</Typography>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => {
